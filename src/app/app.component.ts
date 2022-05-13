b409bbdad28b97703ad04af1510ff0840172092a
Intje Irfan Ibrahim <inceirfanibrahim@gmail.com>
2022-05-13 18:41:41 +0800

    feat: add kartu-kontak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,160 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kartu-kontak';
+  listPerson = [
+    {
+      "index": 1,
+      "picture": "https://picsum.photos/200?random=1",
+      "age": 37,
+      "name": "Flora Nieves",
+      "company": "STUCCO",
+      "phone": "+62 (938) 581-2473",
+      "address": "739 Dewey Place, Deputy, Ohio, 2479"
+    },
+    {
+      "index": 2,
+      "picture": "https://picsum.photos/200?random=2",
+      "age": 33,
+      "name": "Faulkne Tanner",
+      "company": "MANGLO",
+      "phone": "+62 (949) 455-2209",
+      "address": "396 Royce Place, Taft, Texas, 1432"
+    },
+    {
+      "index": 3,
+      "picture": "https://picsum.photos/200?random=3",
+      "age": 33,
+      "name": "Sargent Roth",
+      "company": "CUJO",
+      "phone": "+62 (821) 559-2605",
+      "address": "424 Sedgwick Street, Kyport, Illinois, 7708",
+    },
+    {
+      "index": 4,
+      "picture": "https://picsum.photos/200?random=4",
+      "age": 36,
+      "name": "Staci Keith",
+      "company":"ENERFORCE",
+      "phone": "+62 (891) 542-3727",
+      "address": "569 Ellery Streetm Crawfordsville, Flor: 7162"
+      
+    },
+    {
+      "index": 5,
+      "picture": "https://picsum.photos/200?random=5",
+      "age": 36,
+      "name": "Cline Zimmerman",
+      "company": "BISBA",
+      "phone": "+62 (967 536-2793",
+      "address": "224 Cove Lane, Canoochee, North Carolina, 782"
+    },
+    {
+      "index": 6,
+      "picture": "https://picsum.photos/200?random=6",
+      "age": 26,
+      "name": "Pictra Wade",
+      "company": "ELITA",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 7,
+      "picture":"https://picsum.photos/200?random=7",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+        "index": 8,
+        "picture":"https://picsum.photos/200?random=8",
+        "age": 26,
+        "name": "Higgins Mcdaniel", 
+        "company": "ZYTREK",
+        "phone": "+62 (914) 584-36490",
+        "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 9,
+      "picture":"https://picsum.photos/200?random=9",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 10,
+      "picture":"https://picsum.photos/200?random=10",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 11,
+      "picture":"https://picsum.photos/200?random=11",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 12,
+      "picture":"https://picsum.photos/200?random=12",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 13,
+      "picture":"https://picsum.photos/200?random=13",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    },
+    {
+      "index": 14,
+      "picture":"https://picsum.photos/200?random=14",
+      "age": 26,
+      "name": "Higgins Mcdaniel", 
+      "company": "ZYTREK",
+      "phone": "+62 (914) 584-36490",
+      "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    
+  },
+  {
+    "index": 15,
+    "picture":"https://picsum.photos/200?random=15",
+    "age": 26,
+    "name": "Higgins Mcdaniel",
+    "company": "ZYTREK",
+    "phone": "+62 (914) 584-36490",
+    "address": "796 Independence Avenue, Hachita, Palau, 6767"
+
+  },
+  {
+    "index": 16,
+    "picture":"https://picsum.photos/200?random=16",
+    "age": 26,
+    "name": "Higgins Mcdaniel",
+    "company": "ZYTREK",
+    "phone": "+62 (914) 584-36490",
+    "address": "796 Independence Avenue, Hachita, Palau, 6767"
+    
+  }
+
+
+
+  
+    
+
+  ]
 }
